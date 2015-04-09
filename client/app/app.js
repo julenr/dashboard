ff1347df978b2eca1b01f7d3d3713145fe1e3cc7
julenr <isusk246@gmail.com>
2015-04-09 12:28:32 +1200

    3 commint

--- client/app/app.js
+++ client/app/app.js
@@ -14,14 +14,7 @@ angular.module('dashboardApp', [
         redirectTo: '/'
       });
 
-    $mdIconProvider
-      .defaultIconSet("./assets/svg/avatars.svg", 128)
-      .icon("menu"       , "./assets/svg/menu.svg"        , 24)
-      .icon("share"      , "./assets/svg/share.svg"       , 24)
-      .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
-      .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
-      .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
-      .icon("phone"      , "./assets/svg/phone.svg"       , 512);
+    $mdIconProvider.icon("menu", "/menu.svg", 24);
 
     $mdThemingProvider.theme('default')
       .primaryPalette('blue')
